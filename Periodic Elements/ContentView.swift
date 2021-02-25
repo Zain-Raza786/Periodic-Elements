096f8e1af151fe2c6dc69a0d6dff586f0bd4a718
Zain Raza <zainoakgrove@gmail.com>
2021-02-25 11:00:55 -0600

    Finished app

--- Periodic Elements/ContentView.swift
+++ Periodic Elements/ContentView.swift
@@ -8,9 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var elements = [Element]()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(elements) { element in
+                NavigationLink(
+                    destination: VStack {
+                        Text(element.name)
+                            .padding()
+                        Text(element.symbol)
+                            .padding()
+                        Text(element.history)
+                            .padding()
+                        Text(element.facts)
+                            .padding()
+                    },
+                    label: {
+                        HStack {
+                            Text(element.symbol)
+                            Text(element.name)
+                            
+                        }
+                    })
+            }
+            .navigationTitle("Periodic Elements")
+        }
+        .onAppear(perform: {
+            queryAPI()
+        })
+    }
+    
+    func queryAPI() {
+        let apiKey = "?rapidapi-key=1f4c88104emshef1c1b78b1ea540p19cfeajsn3ef97fdc626c"
+        let query = "https://periodictable.p.rapidapi.com/\(apiKey)"
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                let contents = json.arrayValue
+                for item in contents {
+                    let name = item["name"].stringValue
+                    let symbol = item["symbol"].stringValue
+                    let facts = item["facts"].stringValue
+                    let history = item["history"].stringValue
+                    let element = Element(symbol: symbol, name: name, facts: facts, history: history)
+                    elements.append(element)
+                }
+            }
+            
+        }
     }
 }
 
@@ -18,4 +63,12 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Element: Identifiable {
+    let id = UUID()
+    let symbol: String
+    let name: String
+    let facts: String
+    let history: String
 }
